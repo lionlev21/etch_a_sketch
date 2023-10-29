9e174ca27de9e8adb085d2668e0f46bc51579789
lionlev21 <lionslev21@gmail.com>
2023-10-29 15:44:32 +0200

    Add the goddamn grid

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const slider = document.querySelector(".slider");
 const range = document.querySelector(".range");
+const erase = document.querySelector(".erase");
+let grid = document.querySelector(".grid");
+let size;
 range.innerHTML = `${slider.value}X${slider.value}`;
 slider.addEventListener("input", () => {
   range.innerHTML = `${slider.value}X${slider.value}`;
+  size = slider.value;
 });

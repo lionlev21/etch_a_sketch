706c1de087a16da2de28b5d7a5a9472788a53647
lionlev21 <lionslev21@gmail.com>
2023-10-30 15:07:38 +0200

    Done! Maybe i`ll improve UI in the future

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const slider = document.querySelector(".slider");
 const range = document.querySelector(".range");
-const erase = document.querySelector(".erase");
-let grid = document.querySelector(".grid");
 let size;
-range.innerHTML = `${slider.value}X${slider.value}`;
+let color = "black";
+range.innerHTML = `${slider.value} X ${slider.value}`;
 slider.addEventListener("input", () => {
-  range.innerHTML = `${slider.value}X${slider.value}`;
+  range.innerHTML = `${slider.value} X ${slider.value}`;
   size = slider.value;
 });
+
+function createGrid(x) {
+  const grid = document.querySelector(".grid");
+  const squares = grid.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  grid.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${x}, 1fr)`;
+
+  for (let i = 0; i < x * x; i++) {
+    const square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    grid.insertAdjacentElement("beforeend", square);
+  }
+}
+createGrid(10);
+function changeSize(input) {
+  createGrid(input);
+}
+function colorSquare() {
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+function changeColor(choice) {
+  color = choice;
+}
+function resetBoard() {
+  const grid = document.querySelector(".grid");
+  const squares = grid.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
